a78ec6ad17424bbe1e1f625177495741adf5b734
vatsal dhloakiya <vatsal.dholakiya@gitlab.byteprophecy.com>
2020-09-27 15:32:04 +0530

    image change

--- src/components/ui-components/projectBox/projectBox.jsx
+++ src/components/ui-components/projectBox/projectBox.jsx
@@ -4,7 +4,7 @@ import "./projectBox.scss";
 
 const projectBox = (props) => (
   <div className="portfolio__box">
-    <img src={props.preview} alt="project" />
+    <img src={"https://hello-world-images.s3.ap-south-1.amazonaws.com/water-glass-beginning-inception-wallpaper-preview.jpg"} alt="project" />
     <div className="portfolio__hover-info flex-center">
       <div className="text-center">
         <p className="font30 weight800">{props.title}</p>
